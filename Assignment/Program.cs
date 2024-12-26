51d6bafb361cb214fae665871f5a3d567fffe18d
atlants1998 <atlants.98@gmail.com>
2024-12-27 01:11:31 +0200

    Initial commit OOP 03

---
+++ Assignment/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Assignment OOP 03 !");
+
+            /*Question 1:
+                What is the primary purpose of an interface in C#? */
+            // b) To define a blueprint for a class
+
+
+            /*Question 2:
+                Which of the following is NOT a valid access modifier for interface members in C#? */
+            // a) private
+
+            /*Question 3:
+                Can an interface contain fields in C#? */
+            // b) No
+
+
+            /*Question 4:
+                In C#, can an interface inherit from another interface? */
+            // b) Yes, interfaces can inherit from multiple interfaces
+
+
+            /*Question 5:
+                Which keyword is used to implement an interface in a class in C#? */
+            // NONE
+
+            /*Question 6: 
+                Can an interface contain static methods in C#? */
+            // a) Yes
+
+            /*Question 7:
+                In C#, can an interface have explicit access modifiers for its members? */
+            // a) To hide the interface members from outside access.
+
+            /*Question 9:
+                In C#, can an interface have a constructor? */
+            // b) No, interfaces cannot have constructors
+
+            /*Question 10:
+                How can a C# class implement multiple interfaces? */
+            // c) By separating interface names with commas
+
+
+        }
+    }
+}
